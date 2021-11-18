455755fdd6e3467e584b64ebc238fb7d8d407a53
Xuyang Li <xuyang.li@smail.nju.edu.cn>
2021-11-18 22:44:29 +0800

    basic, without obj & arr index

--- src/main/java/iser21/jmtrace/JMTraceTransformer.java
+++ src/main/java/iser21/jmtrace/JMTraceTransformer.java
@@ -9,6 +9,10 @@ import org.objectweb.asm.commons.Method;
 
 public class JMTraceTransformer implements ClassFileTransformer {
 
+    static boolean inUserPkg(String pkg) {
+        return !(pkg.startsWith("java") || pkg.startsWith("sun") || pkg.startsWith("jdk"));
+    }
+
     public byte[]
     transform(ClassLoader loader,
               String className,
@@ -16,9 +20,8 @@ public class JMTraceTransformer implements ClassFileTransformer {
               ProtectionDomain protectionDomain,
               byte[] classfileBuffer)
             throws IllegalClassFormatException {
-        if (className.startsWith("java") || className.startsWith("sun") 
-            || className.startsWith("jdk")) {
-                return classfileBuffer;
+        if (!inUserPkg(className)) {
+            return null;  // return null is OK
         }
         ClassReader clzReader = new ClassReader(classfileBuffer);
         // first param `clzReader` is for optimization: do not parse when need no transformation
@@ -43,6 +46,9 @@ public class JMTraceTransformer implements ClassFileTransformer {
                 @Override
                 public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
                     super.visitFieldInsn(opcode, owner, name, descriptor);
+                    if (!inUserPkg(owner)) {
+                        return;
+                    }
                     JMTracePrinterGen.instrument(this.mv, opcode, owner, name, descriptor);
                 }
                 // TODO: *ASTORE, *ALOAD
@@ -54,13 +60,38 @@ public class JMTraceTransformer implements ClassFileTransformer {
      * format: R/W thrd-id object-hash access
      */
     public static class JMTracePrinterGen {
-        final static String className =  JMTracePrinterGen.class.getCanonicalName().replace(".", "/");
-
-        final static String desc = "(L)V";
+        final static String className =  "iser21/jmtrace/JMTraceTransformer$JMTracePrinterGen";
+        final static String desc = "(ZLjava/lang/Object;Ljava/lang/String;Ljava/lang/String;I)V";
+        final static String bool_desc = "(Z)V";
         private static void instrument(MethodVisitor methodVisitor, int opcode, String owner, String name, String descriptor) {
             // add param into stack
+            boolean isRead = opcode==Opcodes.GETFIELD||opcode==Opcodes.GETSTATIC;
+            boolean isStatic = opcode == Opcodes.GETSTATIC || opcode == Opcodes.PUTSTATIC;
+            boolean isArray = descriptor.startsWith("[");
+            Object obj = null;
+            if (isRead) {
+                methodVisitor.visitInsn(Opcodes.ICONST_1);
+            } else {
+                methodVisitor.visitInsn(Opcodes.ICONST_0);
+            }
+            if (isStatic) {
+                methodVisitor.visitInsn(Opcodes.ACONST_NULL);
+            } else {
+                methodVisitor.visitInsn(Opcodes.ACONST_NULL);  // TODO: changed to visited object
+            }
+            methodVisitor.visitLdcInsn(owner);
+            methodVisitor.visitLdcInsn(name);
+            if (isArray) {
+                methodVisitor.visitInsn(Opcodes.ICONST_M1);  // TODO: pass array index
+            } else {
+                methodVisitor.visitInsn(Opcodes.ICONST_M1);
+            }
+
             methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, JMTracePrinterGen.className, "printMemoryTrace"
                     , desc,false);
+        }
+        public static void printBool(boolean readFlag) {
+            System.out.println("Here! " + readFlag);
         }
         // will be instrumented with invokestatic to print thrd-id and object id
         public static void printMemoryTrace(boolean readFlag,
@@ -69,18 +100,18 @@ public class JMTraceTransformer implements ClassFileTransformer {
                                       ) {
             String longFieldName = className + '.' + fieldName;
             if (obj != null && arrayIndex < 0) {
-                System.out.printf("%s %d %x%x %s\n", readFlag ? 'R' : 'W',
+                System.out.printf("%s %d %08x%08x %s\n", readFlag ? 'R' : 'W',
                         Thread.currentThread().getId(), System.identityHashCode(obj),
                         System.identityHashCode(longFieldName), longFieldName);
             } else if (obj != null && arrayIndex >= 0) {
-                System.out.printf("%s %d %x%x %s[%d]\n", readFlag ? 'R' : 'W',
+                System.out.printf("%s %d %08x%08x %s[%d]\n", readFlag ? 'R' : 'W',
                         Thread.currentThread().getId(), System.identityHashCode(obj),
                         System.identityHashCode(longFieldName+arrayIndex), longFieldName, arrayIndex);
             } else if (obj == null && arrayIndex < 0) {
-                System.out.printf("%s %d 00000000%x %s\n", readFlag ? 'R' : 'W',
+                System.out.printf("%s %d 00000000%08x %s\n", readFlag ? 'R' : 'W',
                         Thread.currentThread().getId(), System.identityHashCode(longFieldName), longFieldName);
             } else {
-                System.out.printf("%s %d 00000000%x %s[%d]\n", readFlag ? 'R' : 'W',
+                System.out.printf("%s %d 00000000%08x %s[%d]\n", readFlag ? 'R' : 'W',
                         Thread.currentThread().getId(), System.identityHashCode(longFieldName+arrayIndex), longFieldName, arrayIndex);
             }
         }
